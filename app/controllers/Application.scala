67a736e17e105ee97415bcb294900c3a8519c6e0
rexter <rextechnical@gmail.com>
2015-05-17 01:31:18 +0800

    check in site pages with contents

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,51 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
+  def testindex = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  
+  def index = Action {
+    Ok(views.html.base(views.html.partialindex()))
+  }
 
+  def contactus = Action {
+    Ok(views.html.base(views.html.partialcontactus()))
+  }
+  
+  def notfound = Action {
+    Ok(views.html.base(views.html.partial404()))
+  }
+  
+  def aboutus = Action {
+    Ok(views.html.base(views.html.partialaboutus()))
+  }
+  
+  def portfolio = Action {
+    Ok(views.html.base(views.html.partialportfolio()))
+  }
+  
+  def services = Action {
+    Ok(views.html.base(views.html.partialservices()))
+  }
+  
+  def product1 = Action {
+    Ok(views.html.base(views.html.partialproduct1()))
+  }
+  def product2 = Action {
+	  Ok(views.html.base(views.html.partialproduct2()))
+  }
+  def product3 = Action {
+	  Ok(views.html.base(views.html.partialproduct3()))
+  }
+  def product4 = Action {
+	  Ok(views.html.base(views.html.partialproduct4()))
+  }
+  def product5 = Action {
+	  Ok(views.html.base(views.html.partialproduct5()))
+  }
+  def product6 = Action {
+	  Ok(views.html.base(views.html.partialproduct6()))
+  }
+  
 }
